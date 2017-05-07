c249d9ba6a3ccc637cb0477a847296cc11711b06
ytorii <metalwarrior_type_jp@hotmail.com>
2017-05-07 10:30:20 +0900

    Create middleware directory and requestMiddleware

--- front/configureStore.js
+++ front/configureStore.js
@@ -1,24 +1,9 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import { requestGuitar } from './actions/Guitar'
-import { requestUser } from './actions/User'
 import rootReducer from './reducers/index'
+import requestMiddleware from './middlwares/requestMiddleware'
 
-const requestMiddleware = store => next => action => {
-  if(/GUITAR/.test(action.type)){
-    store.dispatch(requestGuitar())
-  } else if(/SIGN/.test(action.type)){
-    store.dispatch(requestUser())
-  }
-  next(action)
-}
-
-const logMiddleware = store => next => action => {
-  console.log(action)
-  next(action)
-}
-
-const middlwares = [ thunk, requestMiddleware, logMiddleware ]
+const middlwares = [ thunk, requestMiddleware ]
 
 const configureStore = (preloadedState) => {
   return createStore(
---
+++ front/middleware/requestMiddleware.js
@@ -0,0 +1,14 @@
+import { requestGuitar } from './actions/Guitar'
+import { requestUser } from './actions/User'
+
+const requestMiddleware = store => next => action => {
+  //console.log(action)
+  if(/GUITAR/.test(action.type)){
+    store.dispatch(requestGuitar())
+  } else if(/SIGN/.test(action.type)){
+    store.dispatch(requestUser())
+  }
+  next(action)
+}
+
+export default requestMiddleware
