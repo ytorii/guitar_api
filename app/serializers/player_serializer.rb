10199b97ecaef11817cb6f7754e977cd1f5a6b8a
ytorii <metalwarrior_type_jp@hotmail.com>
2017-05-10 08:12:26 +0900

    Add flag if user voted the player

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,9 +1,12 @@
 class PlayerSerializer < ActiveModel::Serializer
 
   attributes :id, :guitar_id, :name, :email, :users_votes_count
-  attribute :users_votes, if: :user_voted?
+  attribute :user_voted
 
-  def user_voted?
-    object.id == current_user.id
+  def user_voted
+    if current_user
+      return object.users_votes.any?{ |v| v.user_id == current_user.id }
+    end
+    return false
   end
 end
