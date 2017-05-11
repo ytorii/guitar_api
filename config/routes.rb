f5cccdf98d0638e9e8c57a7259116ee9983fe56a
ytorii <metalwarrior_type_jp@hotmail.com>
2017-05-11 21:59:14 +0900

    Change players route independent from guitars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :guitars do
-    resources :players
+  resources :guitars
+  resources :players do
+    resources :users_vote
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
