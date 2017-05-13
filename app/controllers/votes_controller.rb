0af2d2edb689376c4f3812ad778dc17cce0d6750
ytorii <metalwarrior_type_jp@hotmail.com>
2017-05-13 09:07:27 +0900

    Add votes_controller

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,35 @@
+class VotesController < ApplicationController
+  before_action :set_vote, only: [ :destroy ]
+  #before_action :authenticate_user!
+
+  # POST /votes
+  def create
+    @vote = Vote.new(vote_params.merge(user_id: current_user.id))
+
+    if @vote.save
+      render json: @vote, status: :created
+    else
+      render json: @vote.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /votes/1
+  def destroy
+    if @vote.destroy
+      render json: @vote.id
+    else
+      render json: @vote.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_vote
+      @vote = Vote.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def vote_params
+      params.require(:vote).permit(:player_id)
+    end
+end
