ff8e89b1ed569e99d59d5c42331a1432a3e2dcd2
ytorii <metalwarrior_type_jp@hotmail.com>
2017-05-14 15:34:37 +0900

    Add immutable models

---
+++ front/model/Player.js
@@ -0,0 +1,26 @@
+import { PropTypes } from 'react';
+import { Record } from 'immutable';
+
+const PlayerRecord = Record({
+  id: 0,
+  guitar_id: 0,
+  name: '',
+  group: '',
+  votes_count: 0,
+  user_voted: false
+});
+
+export const PlayerType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  guitar_id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  group: PropTypes.string.isRequired,
+  votes_count: PropTypes.number.isRequired,
+  user_voted: PropTypes.bool.isRequired,
+})
+
+export default class Player extends PlayerRecord {
+  user_voted(){
+    return user_voted
+  }
+}
---
+++ front/model/Vote.js
@@ -0,0 +1,19 @@
+import { PropTypes } from 'react';
+import { Record } from 'immutable';
+
+const VoteRecord = Record({
+  id: 0,
+  guitar_id: 0,
+  user_id: 0,
+  amount: 0
+})
+
+export const VoteType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  guitar_id: PropTypes.number.isRequired,
+  user_id: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired
+})
+
+export default class Vote extends VoteRecord {
+}
